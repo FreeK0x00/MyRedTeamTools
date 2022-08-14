641d5cb34fbc5c61ef90a3847b566412899bd8c0
FreeK0x00 <2694218551@qq.com>
2022-08-14 12:49:03 +0800

    [+] rdpclear.bat

---
+++ 0x08_TraceRemoveTools/rdp_log_clear/rdpclear.bat
@@ -0,0 +1,8 @@
+@echo off
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" /va /f
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers"
+cd %userprofile%\documents\
+c:
+attrib Default.rdp -s -h
+del Default.rdp
